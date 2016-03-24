7157f4e5217ba5c2520b2e882db2f5feab01b9f2
anonymous <nulldreamer@gmail.com>
2016-03-24 15:39:55 -0700

    Changed Class Name, and contents of class

--- src/mergeSort.java
+++ src/mergeSort.java
@@ -5,7 +5,7 @@ import javax.swing.JOptionPane;
 //March 14, 2016
 //Exercise 8-9, Page 331
 
- class GallonsToLiters
+ class HelloWorld
 {
    public static void main(String[] args)
    {
@@ -13,27 +13,9 @@ import javax.swing.JOptionPane;
       int entries;
       String s1;
 
-      s1 = JOptionPane.showInputDialog("Please enter the number of gallons to be converted: ");
+      s1 = JOptionPane.showInputDialog("Hello World: ");
       entries = Integer.parseInt(s1);
 
-      int[] gallons = new int[entries];
 
-      double[] galToLt = new double[entries];
-
-      for (int i = 0; i < entries; i++)
-      {
-         s1 = JOptionPane.showInputDialog("Please enter the number of gallons for item" + (i+1) + ":");
-         gallons[i] = Integer.parseInt(s1);
-         galToLt[i] = gallons[i]*liter;
-      }
-
-      s1 = "Item#     Gallons     Liter Equivalent\n";
-      s1 += "--------    ------------   ------------------------\n";
-      for (int i = 1; i <= entries; i++)
-      {
-         s1 += String.format("  %2d             %4d                %9.3f%n", i, gallons[i-1], galToLt[i-1]);
-      }
-
-      JOptionPane.showMessageDialog(null, s1, "Gallons to Liters", JOptionPane.INFORMATION_MESSAGE);
    }
 }
